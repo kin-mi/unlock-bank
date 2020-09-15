f8b4bcc5f9e694732d487ccf78f7915be627b65c
Kin mi <backtothenearfuture@gmail.com>
2020-09-15 23:17:51 +0900

    initial commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,26 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  theme: {},
+  variants: {},
+  plugins: [require('@tailwindcss/custom-forms')],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.ts',
+    ],
+  },
+}
