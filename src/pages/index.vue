2dbb1528f06b965f91f2cb338ad23355b4b7b913
Kin mi <backtothenearfuture@gmail.com>
2020-09-20 22:31:36 +0900

    fix index design

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,8 +2,7 @@
   <div class="container">
     <h1 class="title">UNLOCK BANK</h1>
     <form
-      class="mx-auto mt-3 py-4 bg-opacity-25 bg-gray-600 rounded-md"
-      style="width: 96%"
+      class="w-full max-w-sm px-2 py-4 bg-opacity-25 bg-gray-600 rounded-b-md"
     >
       <p class="p-1 text-xs text-white font-bold">
         Please input your account number and password.
